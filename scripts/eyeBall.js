08c51ed014ce54096fea809cddb1410cd6ecd6ab
Austin Reach <89221359+Kiikz@users.noreply.github.com>
2021-11-08 15:38:07 -0600

    Add files via upload

---
+++ scripts/eyeBall.js
@@ -0,0 +1,34 @@
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#pword");
+
+togglePassword.addEventListener("click", function (e) {
+  // toggle the type attribute
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+  // toggle the eye slash icon
+  this.classList.toggle("fa-eye-slash");
+});
+
+function checkUsername() {                      
+  var fname = el.value;                        
+  if (fname.length < 6) {                      
+    elMsg.className = 'warning';                  
+    elMsg.textContent = 'Is that your full name?';
+  } else {                                       
+    elMsg.textContent = '';                       
+  }
+}
+
+function tipUsername() {                          // Declare function
+  elMsg.className = 'tip';                        // Change class for message
+  elMsg.innerHTML = 'Please enter your full name.'; // Add message
+}
+
+var el = document.getElementById('fname');     // Username input
+var elMsg = document.getElementById('feedback');  // Element to hold message
+
+// When the username input gains / loses focus call functions above:
+el.addEventListener('focus', tipUsername, false); // focus call tipUsername()
+el.addEventListener('blur', checkUsername, false);// blur call checkUsername()
+
